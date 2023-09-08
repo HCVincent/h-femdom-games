0b6704f9c67fecad484d5a6dbc1e49d5ad0eac9a
HCVincent <lilacsweet@163.com>
2023-09-08 18:40:50 +0800

    put head in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,16 +3,25 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import React from "react";
 import { RecoilRoot, RecoilEnv } from "recoil";
+import Head from "next/head";
 RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   const [theme, setTheme] = React.useState("dark");
   return (
-    <RecoilRoot>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </RecoilRoot>
+    <>
+      <Head>
+        <meta
+          name={process.env.NEXT_PUBLIC_JUICYADS_NAME}
+          content={process.env.NEXT_PUBLIC_JUICYADS_CONTENT}
+        />
+      </Head>
+      <RecoilRoot>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </RecoilRoot>
+    </>
   );
 }
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,12 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head>
-        <meta
-          name={process.env.NEXT_PUBLIC_JUICYADS_NAME}
-          content={process.env.NEXT_PUBLIC_JUICYADS_CONTENT}
-        />
-      </Head>
       <body>
         <Main />
         <NextScript />
