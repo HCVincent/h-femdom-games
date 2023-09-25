7144ae5bced7cda0e451afe4533e544cdc05067d
HCVincent <lilacsweet@163.com>
2023-09-25 18:59:09 +0800

    changes

--- src/components/GamesLists/GamesGridList/GamesGridList.tsx
+++ src/components/GamesLists/GamesGridList/GamesGridList.tsx
@@ -99,14 +99,14 @@ const GamesGridList: React.FC<GamesGridListProps> = ({}) => {
         </div>
       ) : (
         <div className="flex flex-col items-center h-full justify-between mt-10">
-          <div className="flex w-full justify-start">
+          {/* <div className="flex w-full justify-start">
             <Link
               className="btn btn-ghost flex  hover:scale-105 transition-all justify-start text-4xl ml-36 mb-4"
               href={`/games`}
             >
               All Games
             </Link>
-          </div>
+          </div> */}
           <div className="flex flex-col  h-full  gap-y-6 lg:grid lg:grid-cols-3 lg:gap-10">
             {gameStateValue.games.map((game) => (
               <GamesGridItem
