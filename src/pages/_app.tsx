110ffbd9bc7d641fb51152ecfff5bd8a69507242
HCVincent <lilacsweet@163.com>
2023-10-02 06:40:57 +0800

    try favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" sizes="any" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
         <title>AcgFemdom</title>
         <meta
           name={process.env.NEXT_PUBLIC_JUICYADS_NAME}
